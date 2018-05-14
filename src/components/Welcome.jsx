c11490453c6bc9996aa93612793bc2112cf521d0
Vinoth Pandian <vinothpandian@live.com>
2018-05-14 22:13:59 +0200

    background change

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@ import { Grid, Paper, CardMedia, Card, CardHeader } from 'material-ui';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const background = require('../assets/background.jpg');
+const background = require('../assets/background.png');
 
 const Welcome = ({ name }) => (
   <Grid item xs sm={10} md={8} lg={6}>
